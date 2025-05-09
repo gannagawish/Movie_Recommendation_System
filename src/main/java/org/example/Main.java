b661079e25b2950e31575060c8a649d1ebc3f842
OmarMansourAhmed <168648445+OmarMansourAhmed@users.noreply.github.com>
2025-05-09 20:26:11 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,117 +1,116 @@
-package main.java.org.example;
-import java.util.*;
-import java.io.*;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
+package main.java.org.example;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
 import java.util.Vector;
 
-public class Main
-{
+public class Main {
+    public Main() {
+    }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        try {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter("recommendations.txt"))) {
+                CodesCracker scan2 = new CodesCracker();
+                new Vector();
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("recommendations.txt"))) {
-            CodesCracker scan2 = new CodesCracker();
-            Vector<Movie> movies=new Vector<>();
-            try {
-
-                movies=scan2.scanner();
-            }catch (TitleException title){
-                writer.write("ERROR: Movie Title {"+title.title+"} is wrong\n");
-                return;
-            }
-            catch(LetterException idLetter){
-                writer.write("ERROR: Movie Id letters {"+idLetter.id+"} are wrong\n");
-                return;
-            }
-            catch(NumberException idNumber){
-                writer.write("ERROR: Movie Id numbers {"+idNumber.id+"} aren't unique\n");
-                return;
-            }
-
-            //begin of reading of users
-            System.out.print("Enter File Name: ");
-
-            String myfile, userLine, movieLine;
-            Scanner scan = new Scanner(System.in);
-            myfile = scan.nextLine();
-
-            List<User> users = new ArrayList<>();
-            InputHandling inputHandler = new InputHandling();
-
-            // Open output.txt for writing
-            try (BufferedReader bufread = new BufferedReader(new FileReader(myfile))) {
-                // Check if the file is empty
-                File file = new File(myfile);
-                if (file.length() == 0) {
-                    scan.close();
+                Vector movies;
+                try {
+                    movies = CodesCracker.scanner();
+                } catch (TitleException title) {
+                    writer.write("ERROR: Movie Title {" + title.title + "} is wrong");
+                    return;
+                } catch (LetterException idLetter) {
+                    writer.write("ERROR: Movie Id letters {" + idLetter.id + "} are wrong");
+                    return;
+                } catch (NumberException idNumber) {
+                    writer.write("ERROR: Movie Id numbers {" + idNumber.id + "} aren't unique");
                     return;
                 }
 
-                // Read the file two lines at a time
-                while ((userLine = bufread.readLine()) != null) {
-                    // Read the movie id line
-                    movieLine = bufread.readLine();
+                System.out.print("Enter File Name: ");
+                Scanner scan = new Scanner(System.in);
+                String myfile = scan.nextLine();
+                List<User> users = new ArrayList();
+                InputHandling inputHandler = new InputHandling();
 
-                    // Parse the two lines using InputHandling
-                    Object[] parsedInput = inputHandler.parseInput(userLine, movieLine);
-                    if (parsedInput == null) {
-                        // InputHandling already prints parsing errors to console
-                        continue;
-                    }
-
-                    String name = (String) parsedInput[0];
-                    String id = (String) parsedInput[1];
-                    Set<String>IDNumbers_Users=new HashSet<>();
-                    String idNumUser="";
-                    for(int i=0;i<id.length();i++) {
-                        if (Character.isLowerCase(id.charAt(i))) {
-                            writer.write("ERROR: Movie Id letters {" + id + "} are wrong\n");
+                try {
+                    try (BufferedReader bufread = new BufferedReader(new FileReader(myfile))) {
+                        File file = new File(myfile);
+                        if (file.length() == 0L) {
+                            scan.close();
                             return;
                         }
-                        else if(Character.isDigit(id.charAt(i))){
-                            idNumUser +=id.charAt(i);
+
+                        String userLine;
+                        while((userLine = bufread.readLine()) != null) {
+                            String movieLine = bufread.readLine();
+                            Object[] parsedInput = inputHandler.parseInput(userLine, movieLine);
+                            if (parsedInput != null) {
+                                String name = (String)parsedInput[0];
+                                String id = (String)parsedInput[1];
+                                Set<String> IDNumbers_Users = new HashSet();
+                                String idNumUser = "";
+
+                                for(int i = 0; i < id.length(); ++i) {
+                                    if (Character.isLowerCase(id.charAt(i))) {
+                                        writer.write("ERROR: Movie Id letters {" + id + "} are wrong");
+                                        return;
+                                    }
+
+                                    if (Character.isDigit(id.charAt(i))) {
+                                        idNumUser = idNumUser + id.charAt(i);
+                                    }
+                                }
+
+                                if (IDNumbers_Users.contains(idNumUser) || idNumUser.length() != 3) {
+                                    writer.write("ERROR: Movie Id numbers {" + id + "} aren't unique");
+                                    return;
+                                }
+
+                                IDNumbers_Users.add(id);
+                                List<String> movieIDs = (List)parsedInput[2];
+                                User user = new User(name, id, movieIDs);
+                                if (!user.verifyUserName(name)) {
+                                    writer.write("ERROR: User Name '" + name + "' is wrong\n");
+                                    return;
+                                }
+
+                                if (!user.verifyUniqueID(id)) {
+                                    writer.write("ERROR: User Id '" + id + "' is wrong\n");
+                                    return;
+                                }
+
+                                users.add(user);
+                            }
                         }
                     }
-                    if(IDNumbers_Users.contains(idNumUser) || idNumUser.length()!=3){
-                        writer.write("ERROR: Movie Id numbers {"+id+"} aren't unique\n");
-                        return;
-                    }
-                    else IDNumbers_Users.add(id);
-
-                    List<String> movieIDs = (List<String>) parsedInput[2];
-
-                    User user = new User(name, id, movieIDs);
-                    if (!user.verifyUserName(name)) {
-                        writer.write("ERROR: User Name '" + name + "' is wrong\n");
-                        return;
-                    }
-
-                    if (!user.verifyUniqueID(id)) {
-                        writer.write("ERROR: User Id '" + id + "' is wrong\n");
-                        return;
-                    }
-                    users.add(user);
+                } catch (IOException e) {
+                    writer.write("Exception: " + String.valueOf(e) + "\n");
                 }
-            } catch (IOException e) {
-                writer.write("Exception: " + e + "\n");
+
+                scan.close();
+                Recommend rr = new Recommend(users, movies);
+                rr.createGenres();
+                rr.recommendToUsers();
+                rr.outputFile(writer);
             }
-            scan.close();
-
-            //end of users
-            Recommend rr= new Recommend(users,movies);
-            rr.createGenres();
-            rr.recommendToUsers();
-            rr.outputFile(writer);
-
-
         } catch (IOException e) {
-            System.out.println("Error writing to recommendation.txt: " + e);
+            System.out.println("Error writing to recommendation.txt: " + String.valueOf(e));
         }
-
     }
 }
-
