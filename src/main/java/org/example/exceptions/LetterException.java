73ecaa81cfd0f1c95590766e43fd8b64caad62fe
janaalyeldin <159731184+janaalyeldin@users.noreply.github.com>
2025-05-10 23:08:43 +0300

    Delete src directory

--- src/main/java/org/example/exceptions/LetterException.java
+++
@@ -1,9 +0,0 @@
-package main.java.org.example.exceptions;
-
-public class LetterException extends Exception {
-    public String id;
-
-    public LetterException(String id) {
-        this.id = id;
-    }
-}
--- src/main/java/org/example/exceptions/NumberException.java
+++
@@ -1,9 +0,0 @@
-package main.java.org.example.exceptions;
-
-public class NumberException extends Exception {
-    public String id;
-
-    public NumberException(String id) {
-        this.id = id;
-    }
-}
